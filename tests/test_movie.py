cdf2de2d9d464b7d0b1569353791d949a4610a33
Nishat <nsalsabil25@gmail.com>
2022-04-03 17:33:32 -0700

    added source code and tests for Person and Movie

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,36 @@
 import pytest
+from viewing_party.movie import Movie 
 
-def test_1():
+def test_create_movie():
     # Arrange
+    title = "Lion King"
+    genre = "action"
+    rating = 5
+    host = "Netflix"
 
     # Act
+    movie = Movie(title, genre, rating, host)
+    
+    # Assert
+    assert movie.title == title 
+    assert movie.genre == genre
+    assert movie.rating == rating
+    assert movie.host == host 
+
+def test_update_rating_of_movie():
+    # Arrange
+    title = "Lion King"
+    genre = "action"
+    rating = 4
+    host = "Netflix"
+
+
+    # Act
+    movie = Movie(title, genre, rating, host)
+    movie.update_rating(rating)
+
 
     # Assert
-    pass
+    assert movie.rating == 4
+
+
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,62 @@
 import pytest
+from viewing_party.person import Person
+from viewing_party.movie import Movie 
 
-def test_1():
+def test_create_person():
     # Arrange
+    name = "Nishat"
+    watched = []
+    friends = []
+    subscriptions = []
 
     # Act
+    person = Person(name, watched, friends, subscriptions)
+    
+    # Assert
+    assert person.name == name
+    assert person.watched == watched
+    assert person.friends == friends
+    assert person.subscriptions == subscriptions
+
+
+def test_add_watched_movie_to_empty_watched_list():
+    # Arrange
+    name = "Nishat"
+    watched = []
+    friends = []
+    subscriptions = []
+
+    title = "Lion King"
+    genre = "action"
+    rating = 5
+    host = "Netflix"
+
+    # Act
+    person = Person(name, watched, friends, subscriptions)
+    movie = Movie(title, genre, rating, host)
+    person.add_watched(movie)
 
     # Assert
-    pass
+    assert movie in person.watched
+    #assert len(person.watched) == 1
+
+def test_length_of_num_watched():
+    # Arrange
+    name = "Nishat"
+    watched = ["Lion King", ]
+    friends = []
+    subscriptions = []
+
+    title = "Tangled"
+    genre = "comedy"
+    rating = 5
+    host = "Disney Plus"
+
+    # Act
+    person = Person(name, watched, friends, subscriptions)
+    movie = Movie(title, genre, rating, host)
+    person.add_watched(movie)
+
+    # Assert
+    #assert movie in person.watched
+    assert len(person.watched) == 2
